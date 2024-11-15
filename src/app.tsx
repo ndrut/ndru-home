7e6acde72c247d21c8e81789b54096eefd62ceee
Andrew <1191143+ndrut@users.noreply.github.com>
2024-11-15 16:03:17 -0600

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+
+  return (
+    <div class="full-page">
+      <h1 class="creative-name">
+        <span class="light-gray">n</span>
+        <span class="dark-gray">dru</span>
+      </h1>
+    </div>
+  );
+}
