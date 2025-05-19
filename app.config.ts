00ef1d8cf50262fb437faf52239ae7a43f66a20e
Andrew <1191143+ndrut@users.noreply.github.com>
2025-05-19 13:56:41 -0500

    add  cache-control headers for static files

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,27 @@ export default defineConfig(
             compressPublicAssets: true,
             prerender: {
                 crawlLinks: true
-            }
+            },
+            publicAssets: [
+                {
+                    baseURL: "/",
+                    dir: "./public",
+                    maxAge: 60  * 60 * 24 * 7, // 7 days
+                }
+            ],
+            routeRules: {
+                "/fonts/**": {
+                    headers: {
+                      "cache-control": "public, max-age=31536000, immutable",
+                    },
+                  },
+                  "/favicon.ico": {
+                    headers: {
+                      "cache-control": "public, max-age=31536000, immutable",
+                    },
+                  },
+            },
+            
         }
     }
 );
